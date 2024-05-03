b0cb1831b80ad7fb6dbd972a4020a44cd732c5f1
eniangnsa <aeniang@yahoo.com>
2024-05-03 16:16:56 +0700

    added new functions

---
+++ additionals.py
@@ -0,0 +1,25 @@
+# here we define additional functions that will be used in the text evaluator file
+# The approach proposed is to first get the input text, translate it to english and use the metrics
+# for evaluating the english text.
+
+def translate(self):
+    ''''
+    this method detects the language and translates it to russian.
+    '''
+    pass
+
+
+def smog_index(self):
+    pass
+
+def coleman_liau_index(self):
+    pass
+
+def gunning_fog(self):
+    pass
+
+def dale_chall(self):
+    pass
+
+def automated_readability_index(self):
+    pass
